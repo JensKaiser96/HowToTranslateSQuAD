615d99146e05b767ca8fc3367fb86d7ff9afcdb4
JensKaiser96 <jens-kaiser@online.de>
2023-06-27 15:31:47 +0200

    added SQuAD paths, added raw translation path for SQuAD1

--- src/tools/project_paths.py
+++ src/tools/project_paths.py
@@ -1,8 +1,13 @@
 class QADatasetPaths:
     GermanQuADTest = "./data/datasets/GermanQuAD/GermanQuAD_test.json"
     GermanQuADTrain = "./data/datasets/GermanQuAD/GermanQuAD_train.json"
+    SQUAD1_TRAIN = "./data/datasets/SQuAD/train-v1.1.json"
+    SQUAD2_TRAIN = "./data/datasets/SQuAD/train-v2.0.json"
+    SQUAD1_DEV = "./data/datasets/SQuAD/dev-v1.1.json"
+    SQUAD2_DEV = "./data/datasets/SQuAD/dev-v2.0.json"
     MLQA = "./data/datasets/MLQA/test-context-de-question-de.json"
     XQuAD = "./data/datasets/XQuAD/xquad.de.json"
+    RAW_SQUAD1_TRAIN = ".data/datasets/RAW_SQUAD/train-v1.0.json"
 
 
 class StressTestPaths:
