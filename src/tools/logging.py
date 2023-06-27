b3d81f74267001aac5c8c0824f09af9b09e3e4ad
JensKaiser96 <jens-kaiser@online.de>
2023-06-27 12:32:39 +0200

    initail logger commit

---
+++ src/tools/logging.py
@@ -0,0 +1,47 @@
+import logging
+
+from src.tools.io import str_to_safe_path
+
+_default_log_dir = "logs/"
+_default_log_name = "log.log"
+_default_log_path = _default_log_dir + _default_log_name
+_log_path = str_to_safe_path(_default_log_path)
+
+
+def set_log_path(path: str):
+    if _log_path != _default_log_path:
+        logger.warn(
+                f"custom log path already set {_log_path}"
+                f"new path will be respected nonetheless"
+                )
+    _log_path = str_to_safe_path(path)
+
+
+def set_log_name(name: str):
+    set_log_path(_default_log_dir + name)
+
+
+def setup_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+            '%(asctime)s %(name)-12s [%(levelname).1s]: %(message)s',
+            datefmt='%y.%m.%d %H:%M:%S'
+            )
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler(_log_path)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+logger = setup_logger(__name__)
