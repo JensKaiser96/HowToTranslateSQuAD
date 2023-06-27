8369f1320c689892f2a6023e05098e3143816009
Jens Kaiser <jens.kaiser@ims.uni-stuttgart.de>
2023-06-27 15:07:11 +0200

    feat: changed translation model, added logging

--- src/translate/fairseq.py
+++ src/translate/fairseq.py
@@ -1,36 +1,35 @@
 import torch
 
+from src.tools.logging import get_logger
+
+logger = get_logger(__name__)
 
 class Translator:
+    en2de_model_name = "transformer.wmt19.en-de"
+    de2en_model_name = "transformer.wmt19.de-en"
     _repo = "pytorch/fairseq"
     _tokenizer = "moses"
-    _bpe = "subword_nmt"
+    _bpe = "fastbpe"  # "subword_nmt"
 
     def __init__(self):
         self.en2de_model = None
         self.de2en_model = None
 
     def _load_model(self, model_name: str):
+        logger.info(f"Loading translation model:'{model_name}' ...")
         model = torch.hub.load(self._repo, model_name,
+                               checkpoint_file='model1.pt:model2.pt:model3.pt:model4.pt',
                                tokenizer=self._tokenizer, bpe=self._bpe)
         model.eval()  # disable dropout
         model.cuda()  # move model to GPU
         return model
 
-    def _load_en2de_model(self):
-        model_name = 'transformer.wmt16.en-de',
-        self.en2de_model = self._load_model(model_name)
-
-    def _load_de2_en_model(self):
-        model_name = 'transformer.wmt16.de-en',
-        self.en2de_model = self._load_model(model_name)
-
     def en2de(self, text: str) -> str:
         if not self.en2de_model:
-            self._load_en2de_model()
+            self.en2de_model = self._load_model(self.en2de_model_name)
         return self.en2de_model.translate(text)
 
     def de2en(self, text: str) -> str:
         if not self.de2en_model:
-            self._load_de2_en_model()
+            self.de2en_model = self._load_model(self.de2en_model_name)
         return self.de2en_model.translate(text)
