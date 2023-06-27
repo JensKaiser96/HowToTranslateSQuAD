107df09fa84116205a57ef23fb4f1d6afac2ef96
JensKaiser96 <jens-kaiser@online.de>
2023-06-27 16:17:15 +0200

    feat: added verbose option

--- src/tools/io.py
+++ src/tools/io.py
@@ -18,16 +18,17 @@ def _fix_relative_paths(path: str) -> str:
     return path
 
 
-def _rename_old_file(path: str):
+def _rename_old_file(path: str, verbose=False):
     file_name, file_ext = os.path.splitext(path)
     modified_date = datetime.datetime.fromtimestamp(os.path.getmtime(path))
     modified_date_str = modified_date.strftime("%Y_%m_%d_%H_%M_%S")
     new_file_name = f"{file_name}_{modified_date_str}{file_ext}"
-    print(f"file at given path {path} already exists, renaming old file")
+    if verbose:
+        print(f"file at given path {path} already exists, renaming old file")
     os.rename(path, new_file_name)
 
 
-def str_to_safe_path(filepath: str, suffix: str = ""):
+def str_to_safe_path(filepath: str, suffix: str = "", verbose=False):
     fixed_path = _fix_relative_paths(filepath)
     path = Path(fixed_path)
     if not path.suffix:
@@ -39,5 +40,5 @@ def str_to_safe_path(filepath: str, suffix: str = ""):
     path.parent.mkdir(exist_ok=True, parents=True)  # create parent dir
 
     if os.path.exists(path):
-        _rename_old_file(path)
+        _rename_old_file(path, verbose)
     return path
