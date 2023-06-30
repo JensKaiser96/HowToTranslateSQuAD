f6f20258505b494714308c6fe710b201870977ae
JensKaiser96 <jens-kaiser@online.de>
2023-06-30 12:49:05 +0200

    feat: chagend dataset from DEV to TRAIN, added tqdm

--- src/scripts/translate_quad.py
+++ src/scripts/translate_quad.py
@@ -1,14 +1,15 @@
 from src.tools.quad import QUAD
 from src.translate.fairseq import Translator
 from src.tools.logging import get_logger
+import tqdm
 
 logger = get_logger(__file__, script=True)
 
 
 def main():
-    squad1_train = QUAD(QUAD.Datasets.SQUAD1_DEV)  # todo: set to TRAIN instead of DEV
+    squad1_train = QUAD(QUAD.Datasets.SQUAD1_TRAIN)
     translator = Translator()
-    for paragraph in squad1_train.paragraphs:
+    for paragraph in tqdm.tqdm(squad1_train.paragraphs):
         paragraph.context = translator.en2de(paragraph.context)
         for qa in paragraph.qas:
             qa.question = translator.en2de(qa.question)
