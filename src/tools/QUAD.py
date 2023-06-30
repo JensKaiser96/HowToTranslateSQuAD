472cd4e99d2db2fbf9015991e99e5776f7367cbb
JensKaiser96 <jens-kaiser@online.de>
2023-06-30 12:11:49 +0200

    feat/fix: completely changed the data representation so that every access points back to the original dict/json, allows for easy changes to the data and then saving the data without having to convert the object back to a dict. Also added the paths to datasets to the quad class

--- src/tools/QUAD.py
+++ src/tools/QUAD.py
@@ -37,76 +37,144 @@ class QuADKeys:
     question = "question"
     answers = "answers"
     text = "text"
+    answer_start = "answer_start"
 
 
 class Answer:
-    text: str
-    answer_start: int = 0
+    def __init__(self, _data: dict):
+        self._data = _data
 
-    def __init__(self, answer: dict):
-        self.text = answer[QuADKeys.text]
+    @property
+    def text(self) -> str:
+        return self._data[QuADKeys.text]
 
-    def to_dict(self) -> dict:
-        return {
-                QuADKeys.text: self.text
-                }
+    @text.setter
+    def text(self, _text):
+        self._data[QuADKeys.text] = _text
+
+    @property
+    def answer_start(self) -> int:
+        return int(self._data[QuADKeys.answer_start])
+
+    @ answer_start.setter
+    def answer_start(self, _answer_start: int):
+        self._data[QuADKeys.answer_start] = str(_answer_start)
+
+
+class Answers:
+    def __init__(self, _data):
+        self._data = _data
+
+    def __getitem__(self, index) -> Answer:
+        return Answer(self._data[index])
+
+    def __len__(self):
+        return len(self._data)
 
 
 class QA:
-    question: str
-    answers: list[Answer]
+    def __init__(self, _data: dict):
+        self._data = _data
 
-    def __init__(self, qa: dict):
-        self.question = qa[QuADKeys.question]
-        self.answers = [Answer(answer) for answer in qa[QuADKeys.answers]]
+    @property
+    def question(self) -> str:
+        return self._data[QuADKeys.question]
 
-    def to_dict(self) -> dict:
-        return {
-                QuADKeys.question: self.question,
-                QuADKeys.answers: [answer.to_dict() for answer in self.answers]
-                }
+    @question.setter
+    def question(self, _question):
+        self._data[QuADKeys.question] = _question
+
+    @property
+    def answers(self) -> Answers:
+        return Answers(self._data[QuADKeys.answers])
+
+
+class QAS:
+    def __init__(self, _data: list):
+        self._data = _data
+
+    def __getitem__(self, index: int) -> QA:
+        return self._data[index]
+
+    def __len__(self):
+        return len(self._data)
 
 
 class Paragraph:
-    context: str
-    qas: list[QA]
+    def __init__(self, _data: dict):
+        self._data = _data
 
-    def __init__(self, paragraph: dict):
-        self.context = paragraph[QuADKeys.context]
-        self.qas = [QA(qa) for qa in paragraph[QuADKeys.qas]]
+    @property
+    def qas(self) -> QAS:
+        return QAS(self._data[QuADKeys.qas])
 
-    def to_dict(self) -> dict:
-        return {
-                QuADKeys.context: self.context,
-                QuADKeys.qas: [qa.to_dict() for qa in self.qas]
-                }
+    @property
+    def context(self) -> str:
+        return self._data[QuADKeys.context]
+
+    @context.setter
+    def context(self, _context):
+        self._data[QuADKeys.context] = _context
+
+
+class QuadData:
+    def __init__(self, _data: list[Paragraph]):
+        self._data = _data
+
+    def __getitem__(self, index: int) -> Paragraph:
+        return Paragraph(self._data[index][QuADKeys.paragraphs][0])
+
+    def __len__(self):
+        return len(self._data)
 
 
 class QUAD:
     """
     A Object view on the QuAD structure
     """
-    paragraphs: list[Paragraph]
+    class Datasets:
+        GermanQuADTest = "./data/datasets/GermanQuAD/GermanQuAD_test.json"
+        GermanQuADTrain = "./data/datasets/GermanQuAD/GermanQuAD_train.json"
+        SQUAD1_TRAIN = "./data/datasets/SQuAD/train-v1.1.json"
+        SQUAD2_TRAIN = "./data/datasets/SQuAD/train-v2.0.json"
+        SQUAD1_DEV = "./data/datasets/SQuAD/dev-v1.1.json"
+        SQUAD2_DEV = "./data/datasets/SQuAD/dev-v2.0.json"
+        MLQA = "./data/datasets/MLQA/test-context-de-question-de.json"
+        XQuAD = "./data/datasets/XQuAD/xquad.de.json"
+        RAW_SQUAD1_TRAIN = "./data/datasets/RAW_SQUAD/train-v1.0.json"
 
-    def __init__(self, path: str = "", paragraphs: list[Paragraph] = None):
+    class StressTest:
+        OOD = "./data/datasets/stress_test/OOD.json"
+        NOT = "./data/datasets/stress_test/NOT.json"
+        DIS = "./data/datasets/stress_test/DIS.json"
+        ONE = "./data/datasets/stress_test/ONE.json"
+
+    def __init__(self, path: str = "", _data: QuadData = None):
         if path:
-            data_dict = self.load(path)
-            self.paragraphs = [Paragraph(p[QuADKeys.paragraphs][0]) for p in data_dict[QuADKeys.data]]
-        if paragraphs:
-            self.paragraphs = paragraphs
+            self._data = self._load(path)
+        if _data:
+            self._data = {QuADKeys.data: _data}
 
-    def to_dict(self) -> dict:
-        return {QuADKeys.paragraphs: [[paragraph.to_dict() for paragraph in self.paragraphs]]}
+    @property
+    def data(self) -> QuadData:
+        return QuadData(self._data[QuADKeys.data])
+
+    @property
+    def version(self) -> str:
+        return self._data[QuADKeys.version]
+
+    @version.setter
+    def version(self, _version: str):
+        self._data[QuADKeys.version] = _version
 
     @staticmethod
-    def load(path: str) -> dict:
+    def _load(path: str) -> dict:
         with open(path, mode="r", encoding="utf-8") as f_in:
             return json.load(f_in)
 
     def save(self, path: str, version: str = ""):
-        print(f"saving dataset '{version}' of size: '{len(self.paragraphs)}' to path: '{path}'")
-        data = {}
+        print(f"saving dataset '{version}' of size: '{len(self.data)}'"
+              f"to path: '{path}'")
         if version:
-            data[QuADKeys.version] = version
-        data = {QuADKeys.data: self.to_dict()}
-        to_json(data, path)
+            self.version = version
+        to_json(self._data, path)
