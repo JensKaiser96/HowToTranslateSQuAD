da2fa2a4c3b2886fc634979353b342702edc68b0
Jens Kaiser <jens.kaiser@ims.uni-stuttgart.de>
2023-06-30 14:15:15 +0200

    fix: updated QUAD access methods

--- src/scripts/translate_quad.py
+++ src/scripts/translate_quad.py
@@ -9,9 +9,9 @@ logger = get_logger(__file__, script=True)
 def main():
     squad1_train = QUAD(QUAD.Datasets.SQUAD1_TRAIN)
     translator = Translator()
-    for paragraph in tqdm.tqdm(squad1_train.paragraphs):
-        paragraph.context = translator.en2de(paragraph.context)
-        for qa in paragraph.qas:
+    for entry in tqdm.tqdm(squad1_train.data):
+        entry.context = translator.en2de(entry.context)
+        for qa in entry.qas:
             qa.question = translator.en2de(qa.question)
             for answer in qa.answers:
                 answer.text = translator.en2de(answer.text)
