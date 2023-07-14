91ba021cfc118a233980295c9dabaa67d28d8499
JensKaiser96 <jens-kaiser@online.de>
2023-07-14 16:56:56 +0200

    feat: extended test to print out mapping

--- test/unit/tar/test_align.py
+++ test/unit/tar/test_align.py
@@ -3,5 +3,9 @@ from src.tar.align import Aligner
 
 def test_alinger():
     aliner = Aligner()
-    result = aliner("This is a test.", "Dies ist ein Test.")
-    print(result)
+    sentence_en = "Madam President, I would like to confine my remarks to Alzheimer's disease."
+    sentence_de = "Frau Präsidentin, ich möchte meine Bemerkungen auf die Alzheimer-Krankheit beschränken."
+    mapping, tokens_en, tokens_de = aliner(sentence_en, sentence_de)
+
+    for src, trg in mapping:
+        print(f"{sentence_en[src]}\t->\t{sentence_de[trg]}")
