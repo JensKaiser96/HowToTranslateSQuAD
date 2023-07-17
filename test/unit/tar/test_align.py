beac6f287bdfa30d4a117420465d3ba93937b544
JensKaiser96 <jens-kaiser@online.de>
2023-07-17 15:44:32 +0200

    fix: wrong value in test

--- test/unit/tar/test_align.py
+++ test/unit/tar/test_align.py
@@ -25,7 +25,7 @@ def test_surface_token_mapping():
         (8, "confi"): Span(33, 38),
         (9, "ne"): Span(38, 40),
         (10, "my"): Span(41, 43),
-        (11, "re"): Span(7, 9),
+        (11, "re"): Span(44, 46),
         (12, "marks"): Span(46, 51),
         (13, "to"): Span(30, 32),
         (14, "Alzheimer"): Span(55, 64),
