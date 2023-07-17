1fed6ce2a89265d968a5f079ebd6d55afb95d044
JensKaiser96 <jens-kaiser@online.de>
2023-07-17 15:09:46 +0200

    fix: changed test to the surface_token_mapping method

--- test/unit/tar/test_align.py
+++ test/unit/tar/test_align.py
@@ -1,7 +1,37 @@
 from src.tar.align import Aligner
+from src.tar.utils import Span
 
 
-def test_extrapolation():
-    test_sequence = [(1, 1), (2, 2), (4, 4)]
-    gold_sequence = [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)]
-    assert Aligner.extrapolate_alignment(test_sequence) == gold_sequence
+def test_surface_token_mapping():
+    text = (
+        "Madam President , I would like to confine my remarks to Alzheimer 's "
+        "disease .")
+    tokens = [
+        'Mada', 'm', 'President', ',', 'I', 'would', 'like', 'to', 'confi',
+        'ne', 'my', 're', 'marks', 'to', 'Alzheimer', "'", 's', 'disease', '.']
+
+    gold_mapping = {
+        (0, "Mada"): Span(0, 4),
+        (1, "m"): Span(4, 5),
+        (2, "President"): Span(6, 15),
+        (3, ","): Span(15, 16),
+        (4, "I"): Span(17, 18),
+        (5, "would"): Span(19, 24),
+        (6, "like"): Span(25, 29),
+        (7, "to"): Span(30, 32),
+        (8, "confi"): Span(33, 38),
+        (9, "ne"): Span(38, 40),
+        (10, "my"): Span(41, 43),
+        (11, "re"): Span(7, 9),
+        (12, "marks"): Span(46, 51),
+        (13, "to"): Span(30, 32),
+        (14, "Alzheimer"): Span(55, 64),
+        (15, "'"): Span(64, 65),
+        (16, "s"): Span(9, 10),
+        (17, "disease"): Span(67, 74),
+        (18, "."): Span(74, 75)
+    }
+
+    mapping = Aligner.surface_token_mapping(text, tokens)
+
+    assert gold_mapping == mapping
