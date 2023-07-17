32cd97add52109c954673a1b5c9015399af0b287
JensKaiser96 <jens-kaiser@online.de>
2023-07-17 10:52:16 +0200

    wip: feat: code simplification

--- src/tar/utils.py
+++ src/tar/utils.py
@@ -32,34 +32,25 @@ class Span:
         return self.start >= self.end
 
 
-def _extract_wa_from_pi_xi(pi, xi):
-    m, n = pi.size()
-    forward = torch.eye(n)[pi.argmax(dim=1)]
-    backward = torch.eye(m)[xi.argmax(dim=0)]
+def _extract_wa_from_sim(sim: torch.Tensor):
+    m, n = sim.size()
+    forward = torch.eye(n)[sim.argmax(dim=1)]
+    backward = torch.eye(m)[sim.argmax(dim=0)]
     inter = forward * backward.transpose(0, 1)
-    ret = []
-    for i in range(m):
-        for j in range(n):
-            if inter[i, j].item() > 0:
-                ret.append((i, j))
-    return ret
+    return [(i, j) for i, j in (inter > 0).nonzero()]
 
 
-def _sinkhorn_iter(S, num_iter=2) -> tuple[int, int]:
+def _sinkhorn_iter(S: torch.Tensor, num_iter=2) -> torch.Tensor:
     if num_iter <= 0:
         return S, S
     if not S.dim() == 2:
         raise ValueError(
             f"Expected S.dim() to be 2, but was '{S.dim()}' instead \n{S=}")
     S[S <= 0].fill_(1e-6)
-    pi = S
-    xi = pi
-    for i in range(num_iter):
-        pi_sum_over_i = pi.sum(dim=0, keepdim=True)
-        xi = pi / pi_sum_over_i
-        xi_sum_over_j = xi.sum(dim=1, keepdim=True)
-        pi = xi / xi_sum_over_j
-    return pi, xi
+    for _ in range(num_iter):
+        S = S / S.sum(dim=0, keepdim=True)
+        S = S / S.sum(dim=1, keepdim=True)
+    return S
 
 
 def sinkhorn(sim: torch.Tensor, source: Span, target: Span, num_iter=2
@@ -72,8 +63,8 @@ def sinkhorn(sim: torch.Tensor, source: Span, target: Span, num_iter=2
         logger.warn(f"target span is empty: {target.start=}, {target.end=}")
         return []
     sim_wo_offset = sim[source.start: source.end, target.start: target.end]
-    pi, xi = _sinkhorn_iter(sim_wo_offset, num_iter)
-    pred_wa_wo_offset = _extract_wa_from_pi_xi(pi, xi)
+    sim = _sinkhorn_iter(sim_wo_offset, num_iter)
+    pred_wa_wo_offset = _extract_wa_from_pi_xi(sim)
     return [(source_offset + source.start, target_offset + target.start) for
             source_offset, target_offset in pred_wa_wo_offset]
 
