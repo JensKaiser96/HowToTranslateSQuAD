72580c1ce26cdca87d0b63a3e3b478edaff07916
JensKaiser96 <jens-kaiser@online.de>
2023-07-19 11:31:52 +0200

    feat: added ploting results

--- src/scripts/analyze_squad_raw.py
+++ src/scripts/analyze_squad_raw.py
@@ -1,12 +1,14 @@
 from collections import Counter
+import matplotlib.pyplot as plt
 
 from src.qa.quad import QUAD
+from src.io.utils import save_plt
 from src.utils.logging import get_logger
 
 logger = get_logger(__file__, script=True)
 
 
-def main():
+def get_answer_counts():
     squad_raw = QUAD(QUAD.Datasets.Squad1.Translated.Raw.TRAIN)
     answer_counts = Counter()
     for article in squad_raw.data:
@@ -17,6 +19,34 @@ def main():
                     count = context.count(answer.text)
                     answer_counts[count] += 1
     logger.info(answer_counts)
+    return answer_counts
+
+
+def plot_counts(answer_counts):
+    fix, ax = plt.subplots()
+    ax.pie(answer_counts.values())
+    save_plt(plt, "SQuAD1.0_raw_translation_answer_count_distribution")
+
+
+def main():
+    answer_counts = {
+            0: 936,
+            1: 29753,
+            2: 2581,
+            3: 822,
+            4: 301,
+            5: 143,
+            6: 67,
+            7: 59,
+            8: 28,
+            9: 15,
+            10: 11,
+            12: 8,
+            13: 1,
+            17: 1}
+    if not answer_counts:
+        answer_counts = get_answer_counts()
+    plot_counts()
 
 
 if __name__ == '__main__':
