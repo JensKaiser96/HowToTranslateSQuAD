5f9f81312eccdc0b5659f5bb2cf615f42e437f98
Jens Kaiser <jens.kaiser@ims.uni-stuttgart.de>
2023-07-21 10:01:36 +0200

    fix: wip created new quad object to aviod changing the data while iterating over it

--- src/scripts/translate_quad.py
+++ src/scripts/translate_quad.py
@@ -1,4 +1,4 @@
-from src.qa.quad import QUAD
+from src.qa.quad import QUAD, Paragraphs
 from src.tar.translate import Translator
 from src.utils.logging import get_logger
 import tqdm
@@ -8,15 +8,18 @@ logger = get_logger(__file__, script=True)
 
 def main():
     squad1_train = QUAD(QUAD.Datasets.Squad1.TRAIN)
+    translated = QUAD()
     translator = Translator()
-    for entry in tqdm.tqdm(squad1_train.data):
-        for paragraph in tqdm.tqdm(entry):
+    for entry in tqdm.tqdm(squad1_train.data, position=0):
+        translated_entry = Paragraphs(entry._data.copy())
+        for paragraph in tqdm.tqdm(translated_entry, position=1):
             paragraph.context = translator.en2de(paragraph.context)
             for qa in paragraph.qas:
                 qa.question = translator.en2de(qa.question)
                 for answer in qa.answers:
                     answer.text = translator.en2de(answer.text)
-    squad1_train.save(QUAD.Datasets.Squad1.Translated.Raw.TRAIN, "raw")
+        translated.data._data.append(translated_entry._data)
+        translated.save(QUAD.Datasets.Squad1.Translated.Raw.TRAIN, "raw")
 
 
 if __name__ == "__main__":
