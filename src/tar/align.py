798f583bcb9b84f799eb0ef37fe7e32cdb192ffe
JensKaiser96 <jens-kaiser@online.de>
2023-07-25 16:32:09 +0200

    fix: wip refactoring, no more classes

--- src/tar/align.py
+++ src/tar/align.py
@@ -9,86 +9,81 @@ from src.utils.logging import get_logger
 
 logger = get_logger(__name__)
 
+# load and set model config
+_model_config = XLMRobertaConfig.from_pretrained(Alignment.config)
+_model_config.output_hidden_states = True
+_model_config.return_dict = False
 
-class Aligner:
-    # load and set model config
-    _model_config = XLMRobertaConfig.from_pretrained(Alignment.config)
-    _model_config.output_hidden_states = True
-    _model_config.return_dict = False
+# load alignment model
+model = XLMRobertaModel.from_pretrained(
+    Alignment.model_path, config=_model_config)
 
-    # load alignment model
-    model = XLMRobertaModel.from_pretrained(
-        Alignment.model_path, config=_model_config)
 
-    @classmethod
-    def __call__(cls,
-                 source_text: str,
-                 target_text: str,
-                 direction: Direction = Direction.forwards,
-                 ) -> list[tuple[int, int]]:
-        """
-        returns the alignment between the tokens in text_1 and sentence2
-        as well as the tokens in source_text and target_text, without [BOS] and
-        [EOS], combinded with their index in the text to distiglish between
-        tokens with the same string representation
-        """
-        # get encodings, aligner output and spans
-        encoding = Tokenizer.encode(source_text, target_text)
-        output = cls._get_model_output(encoding)
-        source_span, target_span = Tokenizer.split_encoding(encoding)
-        alignments = cls._get_alignments_from_model_output(
-            output, source_span, target_span, direction)
+def align(source_text: str, target_text: str,
+          direction: Direction = Direction.forwards,
+          ) -> list[tuple[int, int]]:
+    """
+    returns the alignment between the tokens in text_1 and sentence2
+    as well as the tokens in source_text and target_text, without [BOS] and
+    [EOS], combinded with their index in the text to distiglish between
+    tokens with the same string representation
+    """
+    # get encodings, aligner output and spans
+    encoding = Tokenizer.encode(source_text, target_text)
+    output = _get_model_output(encoding)
+    source_span, target_span = Tokenizer.split_encoding(encoding)
+    alignments = _get_alignments_from_model_output(
+        output, source_span, target_span, direction)
 
-        # add position (idx) to each token before returning the token list
-        source_tokens = [(idx, token) for idx, token in enumerate(
-            Tokenizer.decode(source_span(encoding)))]
-        target_tokens = [(idx, token) for idx, token in enumerate(
-            Tokenizer.decode(target_span(encoding)))]
+    # add position (idx) to each token before returning the token list
+    source_tokens = [(idx, token) for idx, token in enumerate(
+        Tokenizer.decode(source_span(encoding)))]
+    target_tokens = [(idx, token) for idx, token in enumerate(
+        Tokenizer.decode(target_span(encoding)))]
 
-        for source, target in alignments:
-            logger.debug(
-                f"{source_tokens[source][1]}\t->\t{target_tokens[target][1]}")
+    for source, target in alignments:
+        logger.debug(
+            f"{source_tokens[source][1]}\t->\t{target_tokens[target][1]}")
 
-        return alignments, source_tokens, target_tokens
+    return alignments, source_tokens, target_tokens
 
-    @classmethod
-    def _get_model_output(cls, encoding: dict = None) -> torch.Tensor:
-        with torch.no_grad():
-            _, _, outputs = cls.model(**encoding)
-        output = outputs[8]  # layer 8 has the best alignment
-        # I don't get why this is done, but its in the reference code
-        return torch.bmm(output, output.transpose(1, 2))[0]
 
-    @classmethod
-    def _get_alignments_from_model_output(
-            cls,
-            output: torch.Tensor,
-            source_span: Span,
-            target_span: Span,
-            direction: Direction):
-        # crop array to exclude tokens outside the spans
-        relevant_output = output[source_span.start: source_span.end,
-                                 target_span.start: target_span.end]
-        normalized_output = dimensionalwise_normalize(relevant_output)
+def _get_model_output(encoding: dict = None) -> torch.Tensor:
+    with torch.no_grad():
+        _, _, outputs = model(**encoding)
+    output = outputs[8]  # layer 8 has the best alignment
+    # I don't get why this is done, but its in the reference code
+    return torch.bmm(output, output.transpose(1, 2))[0]
 
-        # actual alignment
-        if direction == Aligner.Direction.forwards:
-            best_match = normalized_output.argmax(axis=1)
-            return [[i, t] for i, t in enumerate(best_match)]
 
-        elif direction == Aligner.Direction.backwards:
-            best_match = normalized_output.argmax(axis=0)
-            return [[i, t] for i, t in enumerate(best_match)]
+def _get_alignments_from_model_output(
+        output: torch.Tensor,
+        source_span: Span,
+        target_span: Span,
+        direction: Direction):
+    # crop array to exclude tokens outside the spans
+    relevant_output = output[source_span.start: source_span.end,
+                             target_span.start: target_span.end]
+    normalized_output = dimensionalwise_normalize(relevant_output)
 
-        elif direction == Aligner.Direction.bidirectional:
-            # sinkhorn algorithm
-            m, n = normalized_output.size()
-            forward = torch.eye(n)[normalized_output.argmax(dim=1)]
-            backward = torch.eye(m)[normalized_output.argmax(dim=0)]
-            inter = forward * backward.T
-            return inter.nonzero()
+    # actual alignment
+    if direction == Direction.forwards:
+        best_match = normalized_output.argmax(axis=1)
+        return [[i, t] for i, t in enumerate(best_match)]
 
-        else:
-            raise ValueError(
-                f"Direction argument must be a Direction, one of: "
-                f"{Aligner.Direction._member_names_}")
+    elif direction == Direction.backwards:
+        best_match = normalized_output.argmax(axis=0)
+        return [[i, t] for i, t in enumerate(best_match)]
+
+    elif direction == Direction.bidirectional:
+        # sinkhorn algorithm
+        m, n = normalized_output.size()
+        forward = torch.eye(n)[normalized_output.argmax(dim=1)]
+        backward = torch.eye(m)[normalized_output.argmax(dim=0)]
+        inter = forward * backward.T
+        return inter.nonzero()
+
+    else:
+        raise ValueError(
+            f"Direction argument must be a Direction, one of: "
+            f"{Direction._member_names_}")
--- src/tar/retrive.py
+++ src/tar/retrive.py
@@ -1,46 +1,44 @@
 from src.tar.utils import Span
 from src.utils.logging import get_logger
-from src.tar.align import Aligner
+from src.tar.align import align
 from src.tar.tokenize import Tokenizer
 
 logger = get_logger(__name__)
 
 
-class Retriver:
-    @classmethod
-    def __call__(cls, source_text: str, source_span: Span,
-                 target_text: str) -> Span:
-        """
-        Given a source text, its answer span and the translation of the
-        source text (target_text), this method returns the answer span
-        of the target_text
-        """
-        # get mapping between source_text and target_text
-        mapping, source_tokens_ids, target_tokens_ids = Aligner(
-            source_text, target_text)
-        logger.debug(f"{mapping=}\n{source_tokens_ids=}\n{target_tokens_ids=}")
+def retrive(cls, source_text: str, source_span: Span,
+            target_text: str) -> Span:
+    """
+    Given a source text, its answer span and the translation of the
+    source text (target_text), this method returns the answer span
+    of the target_text
+    """
+    # get mapping between source_text and target_text
+    mapping, source_tokens_ids, target_tokens_ids = align(
+        source_text, target_text)
+    logger.debug(f"{mapping=}\n{source_tokens_ids=}\n{target_tokens_ids=}")
 
-        # get surface token mapping for both source and target
-        source_surface_token_mapping = Tokenizer.surface_token_mapping(
-            source_text, source_tokens_ids)
-        target_surface_token_mapping = Tokenizer.surface_token_mapping(
-            target_text, target_tokens_ids)
+    # get surface token mapping for both source and target
+    source_surface_token_mapping = Tokenizer.surface_token_mapping(
+        source_text, source_tokens_ids)
+    target_surface_token_mapping = Tokenizer.surface_token_mapping(
+        target_text, target_tokens_ids)
 
-        source_span_token_ids = [index for index, span
-                                 in enumerate(source_surface_token_mapping)
-                                 if span.is_subspan(source_span)]
-        logger.debug(f"{source_span_token_ids}")
+    source_span_token_ids = [index for index, span
+                             in enumerate(source_surface_token_mapping)
+                             if span.is_subspan(source_span)]
+    logger.debug(f"{source_span_token_ids}")
 
-        # get the tokens in the target span
-        mapping_dict = {entry[0]: entry[1] for entry in mapping}
-        # logger.debug(f"{mapping_dict=}")
-        target_span_tokens = [
-            mapping_dict[token_id] for token_id in source_span_token_ids
-            if token_id in mapping_dict]
-        logger.debug(f"{target_span_tokens=}")
+    # get the tokens in the target span
+    mapping_dict = {entry[0]: entry[1] for entry in mapping}
+    # logger.debug(f"{mapping_dict=}")
+    target_span_tokens = [
+        mapping_dict[token_id] for token_id in source_span_token_ids
+        if token_id in mapping_dict]
+    logger.debug(f"{target_span_tokens=}")
 
-        target_surface_spans = [span for index, span
-                                in enumerate(target_surface_token_mapping)
-                                if index in target_span_tokens]
-        logger.debug(f"{target_surface_spans=}")
-        return Span.combine(target_surface_spans)
+    target_surface_spans = [span for index, span
+                            in enumerate(target_surface_token_mapping)
+                            if index in target_span_tokens]
+    logger.debug(f"{target_surface_spans=}")
+    return Span.combine(target_surface_spans)
