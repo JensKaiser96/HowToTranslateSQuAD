66b8a586c63375875eb7f9d8b28acc217509b53c
JensKaiser96 <jens-kaiser@online.de>
2023-07-28 12:00:37 +0200

    feat: wip added Gelectra QA Model wrapper

---
+++ src/qa/gelectra_quad.py
@@ -0,0 +1,74 @@
+import torch
+from transformers.tokenization_utils_base import BatchEncoding
+from transformers.models.electra.tokenization_electra_fast import (
+    ElectraTokenizerFast)
+from transformers.models.electra.modeling_electra import (
+    ElectraForQuestionAnswering)
+
+
+from src.nlp_tools.token import Tokenizer, surface_token_mapping
+from src.nlp_tools.span import Span
+
+
+class Gelectra:
+    _name = "deepset/gelectra-large-germanquad"
+    tokenizer = Tokenizer(ElectraTokenizerFast.from_pretrained(_name))
+    model = ElectraForQuestionAnswering.from_pretrained(_name)
+
+    @classmethod
+    def prompt(cls, context: str, question: str):
+        input = cls.tokenizer.encode(context, question)
+        with torch.no_grad():
+            output = cls.model(**input)
+
+        # get answer on token level
+        answer_start_index = output.start_logits.argmax()
+        answer_end_index = output.end_logits.argmax()
+ 
+        # convert token to surface level
+        context_token_ids, _ = cls.split_encoding(input)
+        context_tokens = cls.tokenizer.decode(context_token_ids(input))
+        mapping = surface_token_mapping(context, context_tokens)
+        return Span.combine(mapping[answer_start_index: answer_end_index + 1])
+
+    @classmethod
+    def split_encoding(cls, encoding: BatchEncoding) -> tuple[Span, Span]:
+        """
+        splits the combined encoding of the ElectraTokenizer of a context
+        question pair into its two spans
+            [[CLS, <context>, PAD, <question>, PAD]]
+        """
+        CLS = cls.tokenizer.t.cls_token_id
+        PAD = cls.tokenizer.t.pad_token_id
+
+        ids = list(encoding.input_ids.flatten())
+
+        # check if sequence is as expected
+        if not ids[0] == CLS:
+            raise ValueError(
+                f"Expected sequence to start with [CLS] token (id:{CLS}), "
+                f"but sequence starts with id:{ids[0]}.")
+        if not ids[-1] == PAD:
+            raise ValueError(
+                f"Expected sequence to end with [PAD] token (id:{PAD}), "
+                f"but sequence ends with id:{ids[-1]}.")
+        if not list(ids).count(PAD) == 2:
+            raise ValueError(
+                f"Expected sequence to have exactly three occurences of the "
+                f"[PAD] token (id:{PAD}), but counted {list(ids).count(PAD)} "
+                f"instead.")
+
+        first_PAD = ids.index(PAD)
+
+        context = Span(1, first_PAD)
+        question = Span(first_PAD + 1, len(ids)-1)
+
+        # Verify spans are not empty
+        if context.is_empty:
+            raise ValueError(
+                f"Source span is not allowed to be empty. {context}")
+        if question.is_empty:
+            raise ValueError(
+                f"Target span is not allowed to be empty. {question}")
+
+        return context, question
