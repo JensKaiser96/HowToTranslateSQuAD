e895990111b7939d49d316d3179a00a7c6a6b90f
kaiserjs <jens.kaiser@vico-consulting.com>
2023-08-08 13:03:58 +0200

    fix: wip added flattening

--- src/qa/train.py
+++ src/qa/train.py
@@ -1,13 +1,35 @@
-from transformers import AutoModelForQuestionAnswering, TrainingArguments, Trainer
-from torch.utils.data import DataLoader
-from transformers import default_data_collator
 import datasets
+from torch.utils.data import DataLoader
+from transformers import AutoModelForQuestionAnswering, TrainingArguments, Trainer
+from transformers import default_data_collator
+
 from src.io.filepaths import Datasets
 from src.qa.gelectra_quad import Gelectra
 
 
-train_dataset = datasets.load_dataset("json", data_files=Datasets.Squad1.Translated.Raw.TRAIN, field="data", split="train")
+def flatten_quad(batch):
+    result = {
+        "id": [],
+        "title": [],
+        "context": [],
+        "question": [],
+        "answers": [],
+    }
+    for entry in batch:
+        for title, sub_entry in zip(entry["title"], entry["paragraphs"]):
+            for qa in sub_entry["qas"]:
+                result["id"].append(qa["id"])
+                result["title"].append(qa["title"])
+                result["context"].append(qa["context"])
+                result["question"].append(qa["question"])
+                result["answers"].append(qa["answers"])
+    return result
+
+
+train_dataset = datasets.load_dataset( "json", data_files=Datasets.Squad1.Translated.Raw.TRAIN, field="data", split="train")
+train_dataset = train_dataset.map(flatten_quad, batched=True, remove_columns=train_dataset.column_names)
 validation_dataset = datasets.load_dataset("json", data_files=Datasets.GermanQuad.TEST, field="data")
+validation_dataset = validation_dataset.map(flatten_quad, batched=True, remove_columns=validation_dataset.column_names)
 train_dataset.set_format("torch")
 validation_dataset.set_format("torch")
 
