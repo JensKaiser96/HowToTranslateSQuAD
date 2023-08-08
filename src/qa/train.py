a2713551125373b8713d21777f0767d921e67b12
kaiserjs <jens.kaiser@vico-consulting.com>
2023-08-08 10:36:31 +0200

    fix: removed dataset editing

--- src/qa/train.py
+++ src/qa/train.py
@@ -3,15 +3,13 @@ from torch.utils.data import DataLoader
 from transformers import default_data_collator
 import datasets
 from src.io.filepaths import Datasets
+from src.qa.gelectra_quad import Gelectra
+
 
 train_dataset = datasets.load_dataset("json", data_files=Datasets.Squad1.Translated.Raw.TRAIN, field="data")
 validation_dataset = datasets.load_dataset("json", data_files=Datasets.GermanQuad.TEST, field="data")
-
-from src.qa.gelectra_quad import Gelectra
-
 train_dataset.set_format("torch")
-validation_set = validation_dataset.remove_columns(["example_id", "offset_mapping"])
-validation_set.set_format("torch")
+validation_dataset.set_format("torch")
 
 train_dataloader = DataLoader(
     train_dataset,
@@ -20,7 +18,7 @@ train_dataloader = DataLoader(
     batch_size=8,
 )
 eval_dataloader = DataLoader(
-    validation_set, collate_fn=default_data_collator, batch_size=8
+    validation_dataset, collate_fn=default_data_collator, batch_size=8
 )
 
 model = AutoModelForQuestionAnswering.from_pretrained("deepset/gelectra-large")
