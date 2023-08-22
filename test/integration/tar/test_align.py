41403eeefe2fd7a84164ff8e8967da7205f7f127
kaiserjs <jens.kaiser@vico-consulting.com>
2023-08-22 15:38:44 +0200

    fix: change to use new code structure

--- test/integration/tar/test_align.py
+++ test/integration/tar/test_align.py
@@ -1,14 +1,13 @@
-from src.tar.align import Aligner
+from src.tar.align import align
 from src.utils.logging import get_logger
 
 logger = get_logger(__name__)
 
 
 def test_alinger():
-    aliner = Aligner()
     sentence_en = "Madam President, I would like to confine my remarks to Alzheimer's disease."
     sentence_de = "Frau Präsidentin, ich möchte meine Bemerkungen auf die Alzheimer-Krankheit beschränken."
-    mapping, tokens_en, tokens_de = aliner.alignment(sentence_en, sentence_de)
+    mapping, tokens_en, tokens_de = align(sentence_en, sentence_de)
 
     for src, trg in mapping:
         logger.info(f"({src}){tokens_en[src]}\t->\t({trg}){tokens_de[trg]}")
