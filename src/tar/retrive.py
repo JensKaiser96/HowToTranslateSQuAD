6155c9d5c07f7bdd86919728e7281b3e9ad09d2b
kaiserjs <jens.kaiser@vico-consulting.com>
2023-08-22 15:41:49 +0200

    chore: fix spelling, remove unnecessary debug logging

--- src/tar/retrive.py
+++ src/tar/retrive.py
@@ -6,7 +6,7 @@ from src.utils.logging import get_logger
 logger = get_logger(__name__)
 
 
-def retrive(source_text: str, source_span: Span, target_text: str) -> Span:
+def retrieve(source_text: str, source_span: Span, target_text: str) -> Span:
     """
     Given a source text, its answer span and the translation of the
     source text (target_text), this method returns the answer span
@@ -14,7 +14,6 @@ def retrive(source_text: str, source_span: Span, target_text: str) -> Span:
     """
     # get mapping between source_text and target_text
     alignment, source_tokens, target_tokens = align(source_text, target_text)
-    logger.debug(f"{alignment=}\n{source_tokens=}\n{target_tokens=}")
 
     # get surface token mapping for both source and target
     source_surface_token_mapping = surface_token_mapping(source_text, source_tokens)
@@ -25,7 +24,6 @@ def retrive(source_text: str, source_span: Span, target_text: str) -> Span:
         for index, span in enumerate(source_surface_token_mapping)
         if span.is_subspan(source_span)
     ]
-    logger.debug(f"{source_span_token_ids}")
 
     # get the tokens in the target span
     # todo: this should work more elegant
@@ -36,12 +34,10 @@ def retrive(source_text: str, source_span: Span, target_text: str) -> Span:
         for token_id in source_span_token_ids
         if token_id in mapping_dict
     ]
-    logger.debug(f"{target_span_tokens=}")
 
     target_surface_spans = [
         span
         for index, span in enumerate(target_surface_token_mapping)
         if index in target_span_tokens
     ]
-    logger.debug(f"{target_surface_spans=}")
     return Span.combine(target_surface_spans)
