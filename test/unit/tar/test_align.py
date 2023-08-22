0833d83486efc4fe5d6d4d6fb76e3a080cfa3c60
kaiserjs <jens.kaiser@vico-consulting.com>
2023-08-22 15:38:07 +0200

    chore: move retrieve tests to own test file

--- test/unit/tar/test_align.py
+++ test/unit/tar/test_align.py
@@ -1,6 +1,5 @@
 from src.nlp_tools.span import Span
-from src.qa.quad import QUAD
-from src.tar.retrive import retrive
+from src.nlp_tools.token import surface_token_mapping
 from src.utils.logging import get_logger
 
 logger = get_logger(__name__)
@@ -57,61 +56,9 @@ def test_surface_token_mapping():
         (19, "."): Span(75, 76),
     }
 
-    mapping = Tokenizer.surface_token_mapping(text, tokens)
+    mapping = surface_token_mapping(text, tokens)
 
     for (index, token), gold_span in gold_mapping.items():
         predicted_span = mapping[index]
         logger.info(f"({index}){token}, Span={predicted_span}")
         assert gold_span == predicted_span
-
-
-def extract_suitable_test_pairs(source_dataset, target_dataset):
-    for source_article, target_article in zip(source_dataset.data, target_dataset.data):
-        for source_paragraph, target_paragraph in zip(source_article, target_article):
-            # extract context
-            source_text = source_paragraph.context
-            target_text = target_paragraph.context
-            # estract answers
-            for source_qa, target_qa in zip(source_paragraph.qas, target_paragraph.qas):
-                for source_answer, target_answer in zip(
-                    source_qa.answers, target_qa.answers
-                ):
-                    # only take ansers where the answer text appears once
-                    if target_text.count(target_answer.text) == 1:
-                        yield (source_text, source_answer, target_text, target_answer)
-
-
-def test_answer_extraction():
-
-    squad = QUAD.Squad1.TRAIN
-    raw_squad = QUAD.Raw.TRAIN
-
-    for test_pairs in extract_suitable_test_pairs(squad, raw_squad):
-        source_text, source_answer, target_text, target_answer = test_pairs
-        logger.info(
-            f"\n ====== Test Case: ====== \n"
-            f"\n ====== Source text: ====== \n"
-            f"{source_text}\n"
-            f"\n ====== Source answer: ====== \n"
-            f"{source_answer.text}\n"
-            f"\n ====== Target text: ====== \n"
-            f"{target_text}\n"
-            f"\n ====== Target answer: ====== \n"
-            f"{target_answer.text}\n"
-        )
-        retrived_span = retrive(
-            source_text, Span.from_answer(source_answer), target_text
-        )
-        logger.info(f"{retrived_span=}")
-        logger.info(
-            f"\n====== Extracted answer ======\n" f"{retrived_span(target_text)}"
-        )
-
-        # fix target_span, at this time (2023-07-21) the answer.answer_start
-        # values are not correctly set
-        target_span = Span(
-            start=target_text.find(target_answer.text),
-            end=target_text.find(target_answer.text) + len(target_answer.text),
-        )
-
-        assert retrived_span == target_span
