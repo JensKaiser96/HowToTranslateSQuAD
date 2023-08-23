8c2bf17163324998f1311c26915da7b45f091667
kaiserjs <jens.kaiser@vico-consulting.com>
2023-08-23 13:52:22 +0200

    chore: moved tests into correct dir, removed duplicates

--- test/integration/tar/test_align.py
+++ test/integration/tar/test_align.py
@@ -1,13 +1,64 @@
-from src.tar.align import align
+from src.nlp_tools.span import Span
+from src.nlp_tools.token import surface_token_mapping
 from src.utils.logging import get_logger
 
 logger = get_logger(__name__)
 
 
-def test_alinger():
-    sentence_en = "Madam President, I would like to confine my remarks to Alzheimer's disease."
-    sentence_de = "Frau Präsidentin, ich möchte meine Bemerkungen auf die Alzheimer-Krankheit beschränken."
-    mapping, tokens_en, tokens_de = align(sentence_en, sentence_de)
+def test_surface_token_mapping():
+    text = (
+        "Madam President, I would like to confine my remarks to Alzheimer's "
+        "disease ."
+    )
+    tokens = [
+        "Mada",
+        "m",
+        "President",
+        ",",
+        "I",
+        "would",
+        "like",
+        "to",
+        "confi",
+        "ne",
+        "my",
+        "re",
+        "marks",
+        "to",
+        "Alzheimer",
+        "'",
+        "s",
+        "disease",
+        "",
+        ".",
+    ]
 
-    for src, trg in mapping:
-        logger.info(f"({src}){tokens_en[src]}\t->\t({trg}){tokens_de[trg]}")
+    gold_mapping = {
+        (0, "Mada"): Span(0, 4),
+        (1, "m"): Span(4, 5),
+        (2, "President"): Span(6, 15),
+        (3, ","): Span(15, 16),
+        (4, "I"): Span(17, 18),
+        (5, "would"): Span(19, 24),
+        (6, "like"): Span(25, 29),
+        (7, "to"): Span(30, 32),
+        (8, "confi"): Span(33, 38),
+        (9, "ne"): Span(38, 40),
+        (10, "my"): Span(41, 43),
+        (11, "re"): Span(44, 46),
+        (12, "marks"): Span(46, 51),
+        (13, "to"): Span(52, 54),
+        (14, "Alzheimer"): Span(55, 64),
+        (15, "'"): Span(64, 65),
+        (16, "s"): Span(65, 66),
+        (17, "disease"): Span(67, 74),
+        (18, ""): Span(75, 75),
+        (19, "."): Span(75, 76),
+    }
+
+    mapping = surface_token_mapping(text, tokens)
+
+    for (index, token), gold_span in gold_mapping.items():
+        predicted_span = mapping[index]
+        logger.info(f"({index}){token}, Span={predicted_span}")
+        assert gold_span == predicted_span
