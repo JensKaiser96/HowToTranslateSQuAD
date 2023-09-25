778d3a97def8855debf4f51e11c0227751cf0e79
kaiserjs <jens.kaiser@vico-consulting.com>
2023-09-25 12:10:41 +0200

    chore: wip remove print

--- src/scripts/analyze/plot_evaluation_histograms.py
+++ src/scripts/analyze/plot_evaluation_histograms.py
@@ -16,7 +16,6 @@ def plot_results(results: Evaluation, name: str, save_path: Path):
     ]
     for metric in metrics:
         data = [getattr(result, metric) for result in results.individual_results]
-        print(data[0])
         # Define the bins
         bins = np.arange(0, 1.1, 0.1)  # Bins from 0 to 1 in 0.1 increments
 
