df303f6833043d80e403cb056b556820219478b8
kaiserjs <jens.kaiser@vico-consulting.com>
2023-09-27 15:07:49 +0200

    feat: finished first version of create_quote.py

---
+++ src/nlp_tools/fuzzy.py
@@ -0,0 +1,15 @@
+from difflib import SequenceMatcher
+from typing import Optional
+
+
+def fuzzy_match(_str: str, options: list[str]) -> Optional[str]:
+    if not _str or not options:
+        return None
+
+    similarities = [SequenceMatcher(a=_str, b=option).ratio() for option in options]
+    max_value = max(similarities)
+    max_index = similarities.index(max_value)
+
+    if similarities.count(max_value) == 1:
+        return options[max_index]
+    return None
