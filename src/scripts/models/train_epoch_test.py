24446cb6ae5db0230597e13e4ac5760908c81e6e
kaiserjs <jens.kaiser@vico-consulting.com>
2023-09-29 14:43:28 +0200

    fix: set correct dataset, remove name arg

--- src/scripts/models/train_epoch_test.py
+++ src/scripts/models/train_epoch_test.py
@@ -6,13 +6,11 @@ from src.utils.logging import get_logger
 
 logger = get_logger(__file__, script=True)
 
-
 train(
     base_model=Gelectra.Base,
-    train_dataset=Dataset.Squad1.TRAIN_SMALL,  # todo set to correct dataset
+    train_dataset=Dataset.Raw.TRAIN_CLEAN,  # todo set to correct dataset
     validation_dataset=Dataset.GermanQUAD.TEST,
-    save_path=MODELS_PATH + "epoch_test/",
-    name="epoch_test",
+    save_path=MODELS_PATH + "gelectra/epoch_test/",
     save_strategy="epoch",
     num_train_epochs=10,
 )
