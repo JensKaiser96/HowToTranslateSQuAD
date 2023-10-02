aedad90fec43be109c6ac3dd8045878bfb306569
kaiserjs <jens.kaiser@vico-consulting.com>
2023-10-02 15:12:49 +0200

    fix: join paths, check for runs dir

--- src/scripts/eval/epoch_eval.py
+++ src/scripts/eval/epoch_eval.py
@@ -6,14 +6,16 @@ from src.utils.logging import get_logger
 
 logger = get_logger(__file__, script=True)
 
-directory_path = "./data/models/gelectra/epoch_test/checkpoints/"
+directory_path = "./data/models/gelectra/epoch_test_2023_09_29_12_05_31/checkpoints/"
 directory_contents = os.listdir(directory_path)
 folders = [
-    item
+    os.path.join(directory_path, item)
     for item in directory_contents
     if os.path.isdir(os.path.join(directory_path, item))
 ]
 
 for checkpoint in folders:
+    if "/runs" in checkpoint:
+        continue
     model = Gelectra(path=checkpoint)
     model.get_evaluation(Dataset.GermanQUAD.TEST)
