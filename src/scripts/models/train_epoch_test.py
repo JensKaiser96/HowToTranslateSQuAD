db52ed792c0f6c3bec38dee7b343520979e8b487
kaiserjs <jens.kaiser@vico-consulting.com>
2023-10-04 15:09:03 +0200

    feat: new hyperparameters for next test

--- src/scripts/models/train_epoch_test.py
+++ src/scripts/models/train_epoch_test.py
@@ -10,7 +10,8 @@ train(
     base_model=Gelectra.Base,
     train_dataset=Dataset.Raw.TRAIN_CLEAN,
     validation_dataset=Dataset.GermanQUAD.TEST,
-    save_path=MODELS_PATH + "gelectra/epoch_test_opt/",
+    save_path=MODELS_PATH + "gelectra/epoch_test_1e-5/",
     save_strategy="epoch",
     num_train_epochs=10,
+    learning_rate=1e-5
 )
