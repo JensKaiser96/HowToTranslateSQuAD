b7f57c54483e4066c48d341af8fd09bf311a8e2c
kaiserjs <jens.kaiser@vico-consulting.com>
2023-10-10 12:19:32 +0200

    feat: created dev dataset

--- src/scripts/dataset/create_dev.py
+++ src/scripts/dataset/create_dev.py
@@ -10,8 +10,8 @@ logger = get_logger(__file__, script=True)
 def main():
     GQUAD_test: Dataset = Dataset.GermanQUAD.TEST
     GQUAD_train: Dataset = Dataset.GermanQUAD.TRAIN
-    GQUAD_dev: Dataset = Dataset.GermanQUAD.DEV
-    GQUAD_train_wo_dev: Dataset = Dataset.GermanQUAD.TRAIN_WO_DEV
+    GQUAD_dev = Dataset(data=[])
+    GQUAD_train_wo_dev = Dataset(data=[])
 
     # Count QA-Pairs in test
     qas_in_test = sum([len(p.qas) for a in GQUAD_test.data for p in a.paragraphs])
@@ -26,7 +26,7 @@ def main():
             threshold = (qas_in_test-current_no_of_qas) / (qas_in_train-qas_seen)
             qas_seen += len(paragraph.qas)
 
-            if random.random() >= threshold:
+            if random.random() <= threshold:
                 GQUAD_dev.data.append(Article(paragraphs=[paragraph]))
                 current_no_of_qas += len(paragraph.qas)
             else:
