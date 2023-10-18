4d3ebff563680cb5d9775f4370cdb865e299d259
kaiserjs <jens.kaiser@vico-consulting.com>
2023-10-18 16:03:23 +0200

    feat: added stats

--- src/scripts/dataset/create_back.py
+++ src/scripts/dataset/create_back.py
@@ -21,8 +21,11 @@ def main(trg_ds: Dataset):
 
     translator = Translator()
 
+    stats = {"pass": 0, "fail": 0}
+
     for trg_article in tqdm.tqdm(trg_ds.data, position=1):
-        for trg_paragraph in tqdm.tqdm(trg_article.paragraphs, position=0):
+        for trg_paragraph in (pbar := tqdm.tqdm(trg_article.paragraphs, position=0)):
+            pbar.set_description(str(stats))
             back_context = translator.de2en(trg_paragraph.context)
             for trg_qa in trg_paragraph.qas:
                 back_question = translator.en2de(trg_qa.question)
@@ -36,13 +39,18 @@ def main(trg_ds: Dataset):
 
                 # like thats ever going to happen, but hey
                 if src_context == back_context and src_question == back_question and src_answer == back_answer:
+                    stats["pass"] += 1
                     back_ds.add_cqa_tuple(trg_paragraph.context, trg_qa.question, trg_qa.answers[0], trg_qa.id)
-                orignial_prediction = english_qa_model.prompt(src_question, src_context)
-                back_prediction = english_qa_model.prompt(back_question, back_context)
-                if compute_f1(orignial_prediction.text, back_prediction.text) >= threshold:
-                    back_ds.add_cqa_tuple(trg_paragraph.context, trg_qa.question, trg_qa.answers[0], trg_qa.id)
+                else:
+                    orignial_prediction = english_qa_model.prompt(src_question, src_context)
+                    back_prediction = english_qa_model.prompt(back_question, back_context)
+                    if compute_f1(orignial_prediction.text, back_prediction.text) >= threshold:
+                        stats["pass"] += 1
+                        back_ds.add_cqa_tuple(trg_paragraph.context, trg_qa.question, trg_qa.answers[0], trg_qa.id)
+                    else:
+                        stats["fail"] += 1
 
-    back_ds.save(Datasets.Squad1.Translated.Raw.TRAIN_BACK, "raw-back")  # todo, set according to dataset
+    back_ds.save(Datasets.Squad1.Translated.Quote.TRAIN_BACK, f"quote-back {stats=}")  # todo, set according to dataset
 
 
 if __name__ == "__main__":
